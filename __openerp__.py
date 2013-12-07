e506fb4229399868a2ef1c97d851b104ab14cba6
Ashok Sahoo <mail@futikismat.com>
2013-12-07 18:14:17 +0530

    basic models created

---
+++ __openerp__.py
@@ -0,0 +1,18 @@
+{
+	'name' : 'Idea',
+	'version' : '1.0'
+	'author' : 'Ashok'
+	'description' : 'Just a new module'
+	'website' : ''
+	'depends' : ['base']
+	'data' : [
+		'security/groups.xml',
+		'security/ir.model.access.csv',
+		'workflow/workflow.xml',
+		'view/views.xml',
+		'wizard/wizard.xml',
+		'report/report.xml',
+	],
+	'demo' : ['demo/demo.xml']
+
+}
